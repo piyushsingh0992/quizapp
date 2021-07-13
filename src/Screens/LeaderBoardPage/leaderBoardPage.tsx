2bfe7370e10095b0395a91027817bd8bb822ceb4
piyushsinghhsv <piyush.singh@hsv.digital>
2021-07-13 16:59:53 +0530

    added leaderboard API Call

--- src/Screens/LeaderBoardPage/leaderBoardPage.tsx
+++ src/Screens/LeaderBoardPage/leaderBoardPage.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./leaderBoardPage.css";
 import Navbar from "../../components/navbar/navbar";
 import LeaderBoard from "../../components/leaderBoard/leaderBoard";
+import { apiCall } from '../../apiCall/apiCall';
+import Loader from '../../components/loader/loader';
+
+
+
+type scoreItemType = {
+    score: number;
+    user: {
+        userName: string,
+        userId: string,
+        password: string
+    }
+
+}
+
+type leaderBoardType = {
+    quizName: string,
+    quizId: string,
+    scoreArray: scoreItemType[]
+}
+
+type leaderBoardTypeArray = leaderBoardType[]
+
+
+
 const LeaderBoardPage = () => {
-    return (
-        <div className="leaderBoardPage">
-            <Navbar />
-            <h1>Leader Board</h1>
-            <div className="leaderBoard-container">
-                <LeaderBoard />
-                <LeaderBoard />
-                <LeaderBoard />
-                <LeaderBoard />
-                <LeaderBoard />
-                <LeaderBoard />
-                <LeaderBoard />
-            </div>
+    const [loader, loaderSetter] = useState<boolean>(true);
+    const [leaderBoardArray, leaderBoardArraySetter] = useState<leaderBoardTypeArray>([]);
+    useEffect(() => {
+        (async function () {
+            try {
+                const response = await apiCall("GET", `leaderBoard`);
+                if (response.success === true) {
+                    leaderBoardArraySetter(response.data.leaderBoards);
+                    loaderSetter(false);
+                }
+
+            } catch (error) {
+                debugger;
+            }
+
+        })()
+    }, [])
+
+
+    return loader ? <Loader /> : <div className="leaderBoardPage">
+        <Navbar />
+        <h1>Leader Board</h1>
+        <div className="leaderBoard-container">
+            {leaderBoardArray.map((item: leaderBoardType) => {
+                
+                return <LeaderBoard quizName={item.quizName} scoreArray={item.scoreArray} />
+            })}
 
         </div>
-    );
+
+    </div>
 };
 
 export default LeaderBoardPage;
--- src/components/leaderBoard/leaderBoard.tsx
+++ src/components/leaderBoard/leaderBoard.tsx
@@ -2,11 +2,26 @@ import React from 'react';
 
 import "./leaderBoard.css";
 
-const leaderBoard = () => {
+type scoreItemType = {
+    score: number;
+    user: {
+        userName: string,
+        userId: string,
+        password: string
+    }
+
+}
+
+type leaderBoardPropType = {
+    quizName: string
+    scoreArray: scoreItemType[]
+}
+
+const leaderBoard = (props: leaderBoardPropType) => {
 
     return (
         <div className="leaderBoard">
-            <h1 >Quiz Name</h1>
+            <h1 >{props.quizName}</h1>
             <table className="leaderBoard-table">
                 <tbody>
                     <tr>
@@ -14,21 +29,16 @@ const leaderBoard = () => {
                         <th>UserName</th>
                         <th>Score</th>
                     </tr>
-                    <tr className="type-b">
-                        <td>2</td>
-                        <td>Eve</td>
-                        <td>94</td>
-                    </tr>
-                    <tr className="type-a">
-                        <td>1</td>
-                        <td>Jill</td>
-                        <td>50</td>
-                    </tr>
-                    <tr className="type-b">
-                        <td>2</td>
-                        <td>Eve</td>
-                        <td>94</td>
-                    </tr>
+                    {props.scoreArray.map((item, index) => {
+                        return <tr className={index % 2 == 0 ? "type-b" : "type-a"}>
+                            <td>{index + 1}</td>
+                            <td>{item.user.userName}</td>
+                            <td>{item.score}</td>
+
+                        </tr>
+                    })}
+
+
                 </tbody>
             </table>
 
