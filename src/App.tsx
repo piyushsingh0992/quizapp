eb233ab5bc392368caf5a4fbd0a248139ec55b8f
piyushsingh0992 <piyushsingh0992@gmail.com>
2021-08-17 12:58:38 +0530

    changed file names

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./app.css";
-import HomePage from "./Screens/homePage";
-import QuizPage from "./Screens/quizPage";
-import LeaderBoardPage from "./Screens/leaderBoardPage";
-import LoginPage from "./Screens/loginPage";
-import Error404Page from "./Screens/error404Page";
+import HomePage from "./screens/homePage";
+import QuizPage from "./screens/quizPage";
+import LeaderBoardPage from "./screens/leaderBoardPage";
+import LoginPage from "./screens/loginPage";
+import Error404Page from "./screens/error404Page";
 import PrivateRoute from "./components/privateRoute";
 import Toast from "./components/toast";
 import ErrorModal from "./components/errorModal";
